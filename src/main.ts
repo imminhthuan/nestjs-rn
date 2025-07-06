030b777ce88c42b8d71788bf711663fd7d583d51
Nguyễn Minh Thuần <banmurom@gmail.com>
2025-07-06 23:29:25 +0700

    commit code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {ConfigService} from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+  const configService = app.get(ConfigService); //chạy trên cổng 8080 gọi đến port trong .env
+  const port = configService.get('PORT');
+
+
+  await app.listen(port);
 }
 bootstrap();
